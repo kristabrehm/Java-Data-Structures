e60fd85ebe09e0f6861c41b84e723138f4be32bd
Krista Brehm <111453866+kristabrehm@users.noreply.github.com>
2023-04-12 10:36:39 -0700

    Create Customer.java

---
+++ Queue/Customer.java
@@ -0,0 +1,33 @@
+public class Customer
+{
+    private int custID;
+    private String category;
+
+    //Constructor to initialize member variables
+    public Customer(int custID, String category)
+    {
+        this.custID = custID;
+        this.category = category;
+    }
+
+    //Accessor method to access its custID
+    public int getCustID()
+    {
+        return custID;
+    }
+
+    //Accessor method to access its category
+    public String getCategory()
+    {
+        return category;
+    }
+
+    //toString method returns a string containing
+    //the information of a customer
+    public String toString( )
+    {
+        String result = "Cust.ID: " + custID
+                + "(" + category + ")";
+        return result;
+    }
+}
